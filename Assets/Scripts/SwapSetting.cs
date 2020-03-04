b20bb3a240c2798c85cb63f510b8dd9c8fcdf705
Andrew Deick <37886715+adeick@users.noreply.github.com>
2020-03-04 12:38:17 -0600

    Cleaned up Elevator --> Platform Transition

--- Assets/Scripts/SwapSetting.cs
+++ Assets/Scripts/SwapSetting.cs
@@ -2,12 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DestroyOffice : MonoBehaviour
+public class SwapSetting : MonoBehaviour
 {
     [SerializeField] private float destroyDelay = 60f;
+    [SerializeField] private GameObject subway;
     // Start is called before the first frame update
     void Start()
     {
+        Invoke("SubwayEnable", destroyDelay);
         Destroy(gameObject, destroyDelay);
     }
+    void SubwayEnable() {
+        subway.SetActive(true);
+    }
 }
+
