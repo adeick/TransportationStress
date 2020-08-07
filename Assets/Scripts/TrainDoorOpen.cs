e09bbb003d56196052042e806d763f8ac9631362
Andrew Deick <37886715+adeick@users.noreply.github.com>
2020-08-07 17:14:27 -0500

    Train Doors now Rotate on Opening
    Also cleaned a bug where shades did not move with doors

--- Assets/Scripts/TrainDoorOpen.cs
+++ Assets/Scripts/TrainDoorOpen.cs
@@ -7,18 +7,21 @@ public class TrainDoorOpen : MonoBehaviour
         
     [SerializeField] float openDelay = 15f;
     [SerializeField] bool mirror = false;
-    [SerializeField] float speed1 = .3f;
-    [SerializeField] float speed2 = .3f;
+    [SerializeField] float speed1 = .25f;
+    [SerializeField] float speed2 = .4f;
 
-    [SerializeField] float openZ1Offset = -0.3f;
-    [SerializeField] float openZ2Offset = -0.2f;
+    [SerializeField] float openZ1Offset = -0.2f;
+    [SerializeField] float openZ2Offset = -0.1f;
+
+    [SerializeField] float rotationAngle = 12f;
+    [SerializeField] float rotationSpeed = 0.3f;// 0 is never and 1 is immediate
 
 //Offset 1 represents the change from the resting position where the doors come out in the Z direction
 //Offset 2 represents the change from the RESTING position where the doors come out in the X direction
                                         //not Offset1
 
     [SerializeField] float openX1Offset = -0.066515f;
-    [SerializeField] float openX2Offset = 1f;
+    [SerializeField] float openX2Offset = .7f;
 
     bool doorsOpening1 = false;
     bool doorsOpening2 = false;
@@ -32,6 +35,7 @@ public class TrainDoorOpen : MonoBehaviour
         if(mirror){
             openX1Offset *= -1;
             openX2Offset *= -1;
+            rotationAngle *= -1;
         }
         Invoke("OpenTrainDoors", openDelay);
     }
@@ -42,6 +46,10 @@ public class TrainDoorOpen : MonoBehaviour
          if(doorsOpening1){
             float step =  speed1 * Time.deltaTime; // calculate distance to move
             transform.position = Vector3.MoveTowards(transform.position, IntermediatePosition, step);
+            //rotate 30degrees?!
+            Quaternion newRotation = Quaternion.AngleAxis(rotationAngle, Vector3.up);
+            transform.rotation= Quaternion.Slerp(transform.rotation, newRotation, rotationSpeed);
+    //
             if(IntermediatePosition == transform.position){
              doorsOpening1 = false;
              doorsOpening2 = true;
@@ -50,6 +58,10 @@ public class TrainDoorOpen : MonoBehaviour
          else if(doorsOpening2){
             float step =  speed2 * Time.deltaTime; // calculate distance to move
             transform.position = Vector3.MoveTowards(transform.position, FinalPosition, step);
+            //rotate back 30 degrees
+            Quaternion newRotation = Quaternion.AngleAxis(0, Vector3.up);
+            transform.rotation= Quaternion.Slerp(transform.rotation, newRotation, 0.1f);
+
             if(FinalPosition == transform.position){
              doorsOpening2 = false;
             }
